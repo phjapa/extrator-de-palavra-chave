ad9da6b568868fe7aeb918464f30791819f34e1b
phjapa <pedro.camargo.souza@escola.pr.gov.br>
2025-06-23 17:31:36 -0300

    ,

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
+    const texto = document.querySelector('#entrada-de-texto')
 }
